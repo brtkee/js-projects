d66507317887efb855f62dc417b69ef649918309
brtkee <150546160+brtkee@users.noreply.github.com>
2024-07-16 00:42:04 +0200

    Update functions.js

--- Flappy Bird Game/functions.js
+++ Flappy Bird Game/functions.js
@@ -10,3 +10,19 @@ export const createImg = (src) => {
 export const randomIntFromRange = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
+
+export const endGame = (animationID, score) => {
+    const tryAgainCard = document.querySelector('.try-again');
+    const scoreEl = document.querySelector('#score');
+    const bestScoreEl = document.querySelector('#best');
+    
+    window.cancelAnimationFrame(animationID);
+
+    if (score >= localStorage.getItem('bestScore')) {
+        localStorage.setItem('bestScore', score);
+    }
+
+    tryAgainCard.classList.add('active');
+    scoreEl.innerText = score;
+    bestScoreEl.innerText = localStorage.getItem('bestScore');
+}
